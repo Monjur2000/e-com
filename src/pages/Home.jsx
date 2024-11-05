1b117d545465bd56511d7e8a6c85222e69e82f80
Monjur2000 <mynamemonjur.com>
2024-11-05 10:06:35 +0530

    complete

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,50 +1,52 @@
-import React from 'react'
-
+import React from 'react';
 
 const Home = () => {
   return (
     <>
-      <section className='2xl:h-[800px] py-0  bg-[#f2f0f1]'>
-        <div className="container">
-          <div className='grid grid-cols-1 md:grid-cols-2  min-h-[60vh] sm:min-h-[70vh]  md:min-h-[80vh] lg:min-h-[90vh] xl:min-h-screen '>
-            {/* HERO SECTION CONTENT START */}
-            <div className='w-[100%] h-full flex items-center py-[30px]'> 
+      <section className='py-0 bg-[#f2f0f1] '>
+        <div className="container overflow-hidden ">
+          <div className='grid grid-cols-1 md:grid-cols-2 md:h-[600px] lg:h-[700px] xl:h-[800px]'>
+            {/* HERO CONTENT */}
+            <div className='w-full py-5 md:h-full overflow-hidden flex items-center justify-start'>
               <div>
-                <h1 className='text-[36px] leading-[37px] md:text-[54px] md:leading-[55px] lg:text-[67px] font-[900] uppercase lg:leading-[64px] mb-2'>Find clothes that matches your style</h1>
-                <p className='text-[14px] text-gray-600 mb-4'>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style</p>
-                <div className='mb-4'>
-                  <button className=' bg-black text-gray-200 px-9 py-3 rounded-[50px]'>Shop Now</button>
-                </div>
-                <div className='grid sm:grid-cols-3 md:grid-cols-2  lg:grid-cols-3'>
-                  <div className='px-2'>
-                    <h2 className='text-[24px] md:text-[34px] lg:text-[44px] font-[700]'>200+</h2>
-                    <p className='text-[14px] text-gray-600'>International Brands</p>
+                  <h1 className='font-[900] uppercase text-[44px] leading-[54px] md:text-[44px] md:leading-[50px] lg:text-[64px] lg:leading-[65px] xl:text-[70px] xl:leading-[75px] 2xl:text-[80px] 2xl:leading-[79px] mb-2'>Find clothes that matches your style</h1>
+
+                  <p className='text-gray-700 text-[16px] leading-5 xl:text-[20px] xl:leading-6 mb-4'>Browse through our diverse range of meticulously crafted garments, designed to bring out your individuality and cater to your sense of style.</p>
+
+                  <div className='w-full sm:w-[200px] bg-black rounded-[50px] mb-4'>
+                    <button className='w-full h-full py-3 text-gray-200'>Shop Now</button>
                   </div>
-                  <div className='px-2'>
-                    <h2 className='text-[24px] md:text-[34px] lg:text-[44px] font-[700]'>2000+</h2>
-                    <p className='text-[14px] text-gray-600'>High-quality Products</p>
+
+                  <div className='grid grid-cols-3 md:grid-cols-2 lg:grid-cols-3'>
+                    <div className='pl-2'>
+                      <h2 className='text-[24px] font-[700] md:text-[28px] lg:text-[32px] xl:text-[34px]'>200+</h2>
+                      <p className='text-gray-700 text-[14px] leading-5 xl:text-[16px] xl:leading-6'>International Brands</p>
+                    </div>
+                    <div className='pl-2'>
+                      <h2 className='text-[24px] font-[700] md:text-[28px] lg:text-[32px] xl:text-[34px]'>2000+</h2>
+                      <p className='text-gray-700 text-[14px] leading-5 xl:text-[16px] xl:leading-6'>High-quality Products</p>
+                    </div>
+                    <div className='pl-2'>
+                      <h2 className='text-[24px] font-[700] md:text-[28px] lg:text-[32px] xl:text-[34px]'>30,000+</h2>
+                      <p className='text-gray-700 text-[14px] leading-5 xl:text-[16px] xl:leading-6'>Happy Customers</p>
+                    </div>
                   </div>
-                  <div className='px-2'>
-                    <h2 className='text-[24px] md:text-[34px] lg:text-[44px] font-[700]'>30,000+</h2>
-                    <p className='text-[14px] text-gray-600'>Happy Customers</p>
-                  </div>
-                </div>
               </div>
             </div>
-            {/* HERO SECTION CONTENT END h*/}
+            {/* HERO CONTENT END */}
 
-            {/* HERO SECTION IMG START */}
-            <div className='w-[100%] h-[70vh] sm:h-[90vh]  md:h-full lg:h-[92vh] xl:h-screen'>
+            {/* HERO IMG START */}
+            <div className='w-full h-[450px] md:h-full overflow-hidden'>
               <figure className='w-full h-full'>
-                <img src="/images/banner.jpeg" alt="" className='w-full h-full object-cover object-top' />
+                <img src="/images/banner.jpeg" alt="" className='w-full h-full object-cover object-top'/>
               </figure>
             </div>
-            {/* HERO SECTION IMG END */}
+            {/* HERO IMG END */}
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
